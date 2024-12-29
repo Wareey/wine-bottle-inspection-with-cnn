9c3b82126d09c6a6981f03bc670c341c4c544692
Ware__ <13882795+te-sela@user.noreply.gitee.com>
2024-12-29 18:15:33 +0800

    基本完成分类算法

--- train.py
+++ train.py
@@ -65,6 +65,10 @@ def main():
     fanzhuanP=0.66  # 随机水平翻转
     num_epochs = 1  # 训练的轮数
 
+    learning_rateA = 0.001  # 学习率
+    fanzhuanPA=0.66  # 随机水平翻转
+    num_epochsA = 1  # 训练的轮数
+
     device=torch.device("cpu")
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -74,12 +78,49 @@ def main():
         transforms.RandomHorizontalFlip(p=fanzhuanP),  # 随机水平翻转
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # 标准化
     ])
+
+    transformA= transforms.Compose([
+        transforms.Resize((224, 224)),  
+        transforms.ToTensor(),  
+        transforms.RandomHorizontalFlip(p=fanzhuanPA),  # 随机水平翻转
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # 标准化
+    ])
+
     t1A,t1B,t2A,t2B=data_split()
     #t1A为瓶盖训练集
     #t1B为标签训练集
 
     #t2A为瓶盖测试集
     #t2B为标签测试集
+    train_dataset1=et(img_dir=t1A,transform=transformA,ca_id=1)
+    train_loader1 = DataLoader(train_dataset1, batch_size=32, shuffle=True)
+    test_dataset1=et(img_dir=t2A,transform=transformA,ca_id=1)
+    test_loader1 = DataLoader(test_dataset1, batch_size=32, shuffle=True)
+    model1 = CNN(num_class=2).to(device)
+
+    train_dataset2=et(img_dir=t1A,transform=transformA,ca_id=2)
+    train_loader2 = DataLoader(train_dataset2, batch_size=32, shuffle=True)
+    test_dataset2=et(img_dir=t2A,transform=transformA,ca_id=2)
+    test_loader2 = DataLoader(test_dataset2, batch_size=32, shuffle=True)
+    model2 = CNN(num_class=2).to(device)
+
+    train_dataset3=et(img_dir=t1A,transform=transformA,ca_id=3)
+    train_loader3 = DataLoader(train_dataset3, batch_size=32, shuffle=True)
+    test_dataset3=et(img_dir=t2A,transform=transformA,ca_id=3)
+    test_loader3 = DataLoader(test_dataset3, batch_size=32, shuffle=True)
+    model3 = CNN(num_class=2).to(device)
+
+    train_dataset4=et(img_dir=t1A,transform=transformA,ca_id=4)
+    train_loader4 = DataLoader(train_dataset4, batch_size=32, shuffle=True)
+    test_dataset4=et(img_dir=t2A,transform=transformA,ca_id=4)
+    test_loader4 = DataLoader(test_dataset4, batch_size=32, shuffle=True)
+    model4 = CNN(num_class=2).to(device)
+
+    train_dataset5=et(img_dir=t1A,transform=transformA,ca_id=5)
+    train_loader5 = DataLoader(train_dataset5, batch_size=32, shuffle=True)
+    test_dataset5=et(img_dir=t2A,transform=transformA,ca_id=5)
+    test_loader5 = DataLoader(test_dataset5, batch_size=32, shuffle=True)
+    model5 = CNN(num_class=2).to(device)
 
     train_dataset6=et(img_dir=t1B,transform=transform,ca_id=6)
     train_loader6 = DataLoader(train_dataset6, batch_size=32, shuffle=True)
@@ -99,10 +140,181 @@ def main():
     test_loader8 = DataLoader(test_dataset8, batch_size=32, shuffle=True)
     model8=CNN(num_class=2).to(device)
 
+    train_dataset9=et(img_dir=t1A,transform=transformA,ca_id=9)
+    train_loader9 = DataLoader(train_dataset9, batch_size=32, shuffle=True)
+    test_dataset9=et(img_dir=t2A,transform=transformA,ca_id=9)
+    test_loader9 = DataLoader(test_dataset9, batch_size=32, shuffle=True)
+    model9=CNN(num_class=2).to(device)
+
+    train_dataset10=et(img_dir=t1A,transform=transformA,ca_id=10)
+    train_loader10 = DataLoader(train_dataset10, batch_size=32, shuffle=True)
+    test_dataset10=et(img_dir=t2A,transform=transformA,ca_id=10)
+    test_loader10 = DataLoader(test_dataset10, batch_size=32, shuffle=True)
+    model10=CNN(num_class=2).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model1.parameters(), lr=learning_rateA)
+    for epoch in range(num_epochsA):
+        for i, (image, label) in enumerate(tqdm(train_loader1)):
+            # image ,label= image.to('cuda'),label.to('cuda')
+            # image ,label= image.to(device),label.to(device)
+
+            outputs = model1(image)
+            # print(f"{output6.size()}*****{label.size()}")
+            loss = criterion(outputs,label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader1)}], Loss: {loss.item():.4f}')
+    
+    model1.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader1:
+            images, labels = data
+            outputs = model1(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'训练了{num_epochsA}轮')
+    print('类型1 Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+    A1= 100 * correct / total
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model2.parameters(), lr=learning_rateA)
+    for epoch in range(num_epochsA):
+        for i, (image, label) in enumerate(tqdm(train_loader2)):
+            # image ,label= image.to('cuda'),label.to('cuda')
+            # image ,label= image.to(device),label.to(device)
+
+            outputs = model2(image)
+            # print(f"{output6.size()}*****{label.size()}")
+            loss = criterion(outputs,label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader2)}], Loss: {loss.item():.4f}')
+    
+    model2.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader2:
+            images, labels = data
+            outputs = model2(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'训练了{num_epochsA}轮')
+    print('类型2 Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+    A2= 100 * correct / total
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model3.parameters(), lr=learning_rateA)
+    for epoch in range(num_epochsA):
+        for i, (image, label) in enumerate(tqdm(train_loader3)):
+            # image ,label= image.to('cuda'),label.to('cuda')
+            # image ,label= image.to(device),label.to(device)
+
+            outputs = model3(image)
+            # print(f"{output6.size()}*****{label.size()}")
+            loss = criterion(outputs,label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader3)}], Loss: {loss.item():.4f}')
+    
+    model3.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader3:
+            images, labels = data
+            outputs = model3(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'训练了{num_epochsA}轮')
+    print('类型3 Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+    A3= 100 * correct / total
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model4.parameters(), lr=learning_rateA)
+    for epoch in range(num_epochsA):
+        for i, (image, label) in enumerate(tqdm(train_loader4)):
+            # image ,label= image.to('cuda'),label.to('cuda')
+            # image ,label= image.to(device),label.to(device)
+
+            outputs = model4(image)
+            # print(f"{output6.size()}*****{label.size()}")
+            loss = criterion(outputs,label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader4)}], Loss: {loss.item():.4f}')
+    
+    model4.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader4:
+            images, labels = data
+            outputs = model4(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'训练了{num_epochsA}轮')
+    print('类型4 Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+    A4= 100 * correct / total
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model5.parameters(), lr=learning_rateA)
+    for epoch in range(num_epochsA):
+        for i, (image, label) in enumerate(tqdm(train_loader5)):
+            # image ,label= image.to('cuda'),label.to('cuda')
+            # image ,label= image.to(device),label.to(device)
+
+            outputs = model5(image)
+            # print(f"{output6.size()}*****{label.size()}")
+            loss = criterion(outputs,label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader5)}], Loss: {loss.item():.4f}')
+    
+    model5.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader5:
+            images, labels = data
+            outputs = model5(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'训练了{num_epochsA}轮')
+    print('类型5 Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+    A5= 100 * correct / total
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model6.parameters(), lr=learning_rate)
-
     print(torch.cuda.is_available())
     for epoch in range(num_epochs):
         for i, (image, label) in enumerate(tqdm(train_loader6)):
@@ -201,10 +413,83 @@ def main():
     100 * correct / total))
     A8= 100 * correct / total
 
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model9.parameters(), lr=learning_rateA)
+    for epoch in range(num_epochsA):
+        for i, (image, label) in enumerate(tqdm(train_loader9)):
+            # image ,label= image.to('cuda'),label.to('cuda')
+            # image ,label= image.to(device),label.to(device)
+
+            outputs = model9(image)
+            # print(f"{output6.size()}*****{label.size()}")
+            loss = criterion(outputs,label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader9)}], Loss: {loss.item():.4f}')
+    
+    model9.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader9:
+            images, labels = data
+            outputs = model9(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'训练了{num_epochsA}轮')
+    print('类型9 Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+    A9= 100 * correct / total
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model10.parameters(), lr=learning_rateA)
+    for epoch in range(num_epochsA):
+        for i, (image, label) in enumerate(tqdm(train_loader10)):
+            # image ,label= image.to('cuda'),label.to('cuda')
+            # image ,label= image.to(device),label.to(device)
+
+            outputs = model10(image)
+            # print(f"{output6.size()}*****{label.size()}")
+            loss = criterion(outputs,label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader10)}], Loss: {loss.item():.4f}')
+    
+    model10.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader10:
+            images, labels = data
+            outputs = model10(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'训练了{num_epochsA}轮')
+    print('类型10 Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+    A10= 100 * correct / total
+
+
     print(f'学习率为{learning_rate} 训练了{num_epochs}轮  随机翻转率{fanzhuanP}')
+    print(f'瑕疵1 测试集准确率{A1}')
+    print(f'瑕疵2 测试集准确率{A2}')
+    print(f'瑕疵3 测试集准确率{A3}')
+    print(f'瑕疵4 测试集准确率{A4}')
+    print(f'瑕疵5 测试集准确率{A5}')
     print(f'瑕疵6 测试集准确率{A6}')
     print(f'瑕疵7 测试集准确率{A7}')
     print(f'瑕疵8 测试集准确率{A8}')
+    print(f'瑕疵9 测试集准确率{A9}')
+    print(f'瑕疵10 测试集准确率{A10}')
+    
     
     logging.basicConfig(
     filename='train.log',  # 日志文件名
@@ -212,8 +497,16 @@ def main():
     format='%(asctime)s - %(levelname)s - %(message)s'  # 日志格式
     )
     logging.info(f'学习率为{learning_rate} 训练了{num_epochs}轮  随机翻转率{fanzhuanP}')
+    logging.info(f'瑕疵1 测试集准确率{A1}')
+    logging.info(f'瑕疵2 测试集准确率{A2}')
+    logging.info(f'瑕疵3 测试集准确率{A3}')
+    logging.info(f'瑕疵4 测试集准确率{A4}')
+    logging.info(f'瑕疵5 测试集准确率{A5}')
     logging.info(f'瑕疵6 测试集准确率{A6}')
     logging.info(f'瑕疵7 测试集准确率{A7}')
     logging.info(f'瑕疵8 测试集准确率{A8}')
+    logging.info(f'瑕疵9 测试集准确率{A9}')
+    logging.info(f'瑕疵10 测试集准确率{A10}')
+
 if __name__ == '__main__':
     main()
